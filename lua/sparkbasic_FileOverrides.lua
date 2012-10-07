f50347b53e869632480152c0c148d3087f6bb390
Alex Hayton <alex.hayton@gmail.com>
2012-10-07 23:55:05 +0100

    Added FileOverrides.lua and some initial values.

---
+++ lua/sparkbasic_FileOverrides.lua
@@ -0,0 +1,54 @@
+//________________________________
+//
+//   	NS2 Combat Mod     
+//	Made by JimWest and MCMLXXXIV, 2012
+//
+//________________________________
+
+// combat_FileOverrides.lua
+
+// Use this to remove files entirely from the loading process.
+
+// Case doesn't matter here!
+kSparkBasicFileRemovals = {
+	"lua/NS2Gamerules.lua",
+	"lua/MarineTeam.lua",
+	"lua/Marine.lua",
+	"lua/MarineSpectator.lua",
+	"lua/AlienTeam.lua",
+	"lua/Alien.lua",
+	"lua/AlienSpectator.lua",
+	"lua/ResourcePoint.lua",
+	"lua/ResourceTower.lua"
+}
+
+// Case matters here!
+kSparkBasicEntityStubs = {
+}
+
+if #kCombatFileRemovals > 0 then
+	Shared.Message ("Registering file removals...")
+end
+
+for index, override in ipairs(kCombatFileRemovals) do
+	Shared.Message ("Removing source file " .. override)
+	
+	// Hook into the load tracker code to remove the file when we come across it.
+	// The normalized string is always lower case.
+	override = string.lower(override)
+	LoadTracker:SetFileOverride(override, "")
+end
+
+for index, stub in ipairs(kCombatEntityStubs) do
+	Shared.Message ("Stubbing the entity " .. stub)
+	
+	local result, error 
+	result, error = loadstring(stub .. " = {}")
+	pcall(result)
+	
+	result, error = loadstring(stub .. ".kModelName = \"\"")
+	pcall(result)
+	
+	result, error = loadstring(stub .. ".kMapName = \"\"")
+	pcall(result)
+end 
